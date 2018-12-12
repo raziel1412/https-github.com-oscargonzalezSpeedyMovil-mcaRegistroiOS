e98d8354a5f196ec45a7bf06e03dc7d69233cd4e
pil4r <pilarr.pz@gmail.com>
2018-12-12 14:14:24 -0600

    mcaRegistroiOS project added.

---
+++ mcaRegistroiOS/mcaRegisterManager.swift
@@ -0,0 +1,26 @@
+//
+//  mcaRegisterManager.swift
+//  mcaRegistroiOS
+//
+//  Created by Pilar del Rosario Prospero Zeferino on 12/12/18.
+//  Copyright © 2018 Speedy Movil. All rights reserved.
+//
+
+import UIKit
+
+public enum RegisterType {
+    case General
+    case Custom
+}
+
+public class mcaRegisterManager: NSObject {
+    
+    public class func launch(navController: UINavigationController?, typeRegister: RegisterType) {
+        if typeRegister == .General {
+            
+        } else {
+            
+        }
+    }
+
+}
---
+++ mcaRegistroiOS/mcaRegistroiOS.h
@@ -0,0 +1,19 @@
+//
+//  mcaRegistroiOS.h
+//  mcaRegistroiOS
+//
+//  Created by Pilar del Rosario Prospero Zeferino on 12/12/18.
+//  Copyright © 2018 Speedy Movil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mcaRegistroiOS.
+FOUNDATION_EXPORT double mcaRegistroiOSVersionNumber;
+
+//! Project version string for mcaRegistroiOS.
+FOUNDATION_EXPORT const unsigned char mcaRegistroiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mcaRegistroiOS/PublicHeader.h>
+
+
